7867ba1471eac722e758a9b829e91bd435efd99b
vaniaa1 <145036392+vaniaa1@users.noreply.github.com>
2023-09-17 20:44:24 -0600

    Update holamundo.py

--- holamundo.py
+++ holamundo.py
@@ -1,2 +1,32 @@
+1:print hola mundo
 #en este programa voy a desplegar un hola mundo
 print("hola mundo")
+
+2:maquina de funciones 
+def f(x): #haciendo mi cajita feliz
+  f_x=abs(3*x)
+  return f_x
+
+y= f(10) #pucharle play con 10 de input 
+
+priny(y) #imprimiento nuestro output en la consola
+
+3: coding rush
+"""clima?"""
+def funcion_feliz():
+  print("¿te gustaria saber como esta el clima hoy?")
+  si=input()
+  print("te lo dire en tres escalas")
+  print("elige el primero que más te guste, Celcius, frenheit y Kelvin")
+  celcius=input()
+  print("el clima en celcius es de 18 grados")
+  ok=input()
+  farenheit=input()
+  print("el clima en farenheit es de 65 grados")
+  vava=input()
+  kelvin=input()
+  print("el clima en kelvin esta a 8 grados")
+  gracias=input()
+  print("de nada")
+
+
